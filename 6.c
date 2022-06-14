2708d8df469178f37c1afa777a7b1a0d2c5d3a6b
Marcos Alves <77404505+SoulSocram@users.noreply.github.com>
2022-06-14 14:52:18 -0300

    14:52 - 14/06/2022

---
+++ 6.c
@@ -0,0 +1,40 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+main() {
+    float porcentagemFeminina;
+    int idade, maiorIdade, contPorcentagem, cont;
+    char sexo[40], corOlhos[40], corCabelos[40];
+
+    contPorcentagem = 0;
+    maiorIdade = 0;
+    cont = 0;
+
+    do {
+        
+        cont++;
+        printf("Informe o sexo (feminino | masculino):\n");
+        scanf("%s", &sexo);
+        printf("Informe a cor dos olhos(azuis | verdes | castanhos | pretos):\n");
+        scanf("%s", &corOlhos);
+        printf("Informe a cor dos cabelos(castanhos | loiros | ruivos):\n");
+        scanf("%s", &corCabelos);
+        printf("Informe a idade:\n");
+        scanf("%d", &idade);
+
+        if ((strcmp (sexo,"feminino") == 0) && ((idade >= 18) && (idade <= 35)) && (strcmp (corOlhos,"verdes") == 0) && (strcmp (corCabelos,"loiros") == 0)) {
+            contPorcentagem++;
+        }
+
+        if (idade > maiorIdade) {
+            maiorIdade = idade;
+        }
+    }
+    while (idade != -1);
+
+    printf("Maior idade: %d\n", maiorIdade);
+    porcentagemFeminina = (100 * contPorcentagem)/cont;
+    printf("Porcentagem feminina com olhos verdes, cabelos louros que estão entre 18 e 35 anos: %.2f\n", porcentagemFeminina);
+}
